6326ba1b71f1c7422be6ae1c60f8fcc6381cfbd4
VS2020-GossipSim <kkoslowski38@gmail.com>
2020-08-31 12:29:48 +0200

    project sources

---
+++ src/gossip/HealthyNodeInfo.java
@@ -0,0 +1,32 @@
+package gossip;
+
+/**
+ * Contains information about a healthy node.
+ * Healthy nodes are active processes and have an increasing heartbeat.
+ */
+public class HealthyNodeInfo extends NodeInfo {
+    /**
+     * Creates healthy node info for given process id.
+     * @param processId process id
+     */
+    public HealthyNodeInfo(int processId) {
+        super(processId);
+    }
+
+    /**
+     * Copies node information.
+     * @param nodeInfo node info
+     */
+    public HealthyNodeInfo(NodeInfo nodeInfo) {
+        super(nodeInfo);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return super.equals(object);
+    }
+
+    public String toString() {
+        return "Process " + getProcessId() + " is ACTIVE.";
+    }
+}
